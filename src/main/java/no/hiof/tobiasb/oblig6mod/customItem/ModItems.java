6b0adb3a9a311a335d1a07c54ced7995cf6eed39
Tobias Bragvin <tobiasbragvin@Tobias-MacBook-Pro.local>
2023-04-20 21:20:17 +0200

    Initial commit

---
+++ src/main/java/no/hiof/tobiasb/oblig6mod/customItem/ModItems.java
@@ -0,0 +1,9 @@
+package no.hiof.tobiasb.oblig6mod.customItem;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+  //  public static final DeferredRegister<Item> ITEMS =
+            //DeferredRegister.create(ForgeRegistries.ITEMS, )
+}
