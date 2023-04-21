68961b3c67e151eb3a534f5bd8c2986c39fd566d
bragvin <tobyplays@icloud.com>
2023-04-21 11:36:25 +0200

    a few changes

--- src/main/java/no/hiof/tobiasb/oblig6mod/customItem/ModFoods.java
+++ src/main/java/no/hiof/tobiasb/oblig6mod/customItem/ModFoods.java
@@ -5,7 +5,13 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties AMOGUS = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.8F).effect(new MobEffectInstance(MobEffects.REGENERATION, 8000, 5), 1.0F).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 8000, 2), 1.0F).effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 8000, 2), 1.0F).effect(new MobEffectInstance(MobEffects.ABSORPTION, 8000, 5), 1.0F).effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 10000, 5), 1.0F).alwaysEat().build();
-
+    public static final FoodProperties AMOGUS = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.8F).
+            effect(new MobEffectInstance(MobEffects.REGENERATION, 8000, 5), 1.0F).
+            effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 8000, 2), 1.0F).
+            effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 8000, 2), 1.0F).
+            effect(new MobEffectInstance(MobEffects.ABSORPTION, 8000, 5), 1.0F).
+            effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 10000, 5), 1.0F).
+            alwaysEat().build();
+        // Koden her er som sagt hentet fra Vanilla Minecraft, og Enchanted Golden Apple. Her har jeg bare endret på verdiene, og lagt til en til.
 
 }
